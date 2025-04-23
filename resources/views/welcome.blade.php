c2d79a3893f954af805de1f72f88a12e6673be1f
Angelo van Osch <Angelo.van.osch@hotmail.com>
2025-04-24 00:29:50 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+  </head>
+  <body>
+    <h1 class="text-4xl text-red-600 font-bold">Tailwind is working! 🎉</h1>
+  </body>
+</html>
