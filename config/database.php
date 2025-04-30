1493ca2e5bab81ffb67d942c98c7e8b5be0e5ab5
Angelo van Osch <Angelo.van.osch@hotmail.com>
2025-04-30 16:02:43 +0200

    added testing env

--- config/database.php
+++ config/database.php
@@ -35,7 +35,7 @@ return [
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'training_project'),
+            'database' => env('DB_DATABASE', env('APP_ENV') == 'testing' ? 'training_project_testing' : 'training_project'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
@@ -84,7 +84,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
