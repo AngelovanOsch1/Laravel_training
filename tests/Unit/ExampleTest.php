f2e093581cd480931b12f1f95a7f04e252388f2b
Angelo van Osch <Angelo.van.osch@hotmail.com>
2025-05-01 11:16:26 +0200

    finished unit testing

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,10 +3,11 @@
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 
 class ExampleTest extends TestCase
 {
-    /** @test */
+    #[Test]
     public function basic_test()
     {
         $this->assertTrue(true);
