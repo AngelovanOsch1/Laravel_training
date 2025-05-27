49185a370c64d96c1e4c0c52fbf7e8188bf92fe7
Angelo van Osch <Angelo.van.osch@hotmail.com>
2025-05-27 11:02:54 +0200

    Forgot Test import lol

--- tests/Feature/DeleteSeriesTest.php
+++ tests/Feature/DeleteSeriesTest.php
@@ -8,12 +8,14 @@ use App\Models\Series;
 use Livewire\Livewire;
 use App\Models\SeriesUser;
 use App\Livewire\UserSeriesList;
+use PHPUnit\Framework\Attributes\Test;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class DeleteSeriesTest extends TestCase
 {
     use RefreshDatabase;
 
+    #[Test]
     public function it_deletes_a_series_user_record()
     {
         $user = User::factory()->create();
