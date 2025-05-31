4c3a07db84fae03ecf0ab3aef021132e1d27be1d
Angelo van Osch <Angelo.van.osch@hotmail.com>
2025-05-31 20:37:10 +0200

    You can see your top 10 series in your profile page

--- app/Livewire/UserHighestRatedEntries.php
+++ app/Livewire/UserHighestRatedEntries.php
@@ -2,12 +2,27 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
+use App\Support\GlobalHelper;
 
 class UserHighestRatedEntries extends Component
 {
+    public User $user;
+
+    public function mount()
+    {
+        $this->user = GlobalHelper::getLoggedInUser();
+    }
+
     public function render()
     {
-        return view('livewire.user-highest-rated-entries');
+        $topRatedSeries = $this->user
+            ->series()
+            ->orderByPivot('score', 'desc')
+            ->take(10)
+            ->get();
+
+        return view('livewire.user-highest-rated-entries', ['topRatedSeries' => $topRatedSeries]);
     }
 }
--- resources/views/livewire/user-highest-rated-entries.blade.php
+++ resources/views/livewire/user-highest-rated-entries.blade.php
@@ -1,29 +1,15 @@
 <div class="my-10 w-full rounded-xl bg-white px-6 py-6 shadow-lg shadow-[#e0e0e0]">
     <h3 class="text-xl font-bold mb-4">Top Rated Anime</h3>
-
-    <div class="flex flex-wrap gap-4">
-        <img src="https://cdn.myanimelist.net/images/anime/10/47347.jpg" alt="Attack on Titan"
-            class="w-20 h-28 object-cover rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300" />
-        <img src="https://cdn.myanimelist.net/images/anime/3/40451.jpg" alt="Death Note"
-            class="w-20 h-28 object-cover rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300" />
-        <img src="https://cdn.myanimelist.net/images/anime/1208/94745.jpg" alt="Demon Slayer"
-            class="w-20 h-28 object-cover rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300" />
-        <img src="https://cdn.myanimelist.net/images/anime/4/19644.jpg" alt="Fullmetal Alchemist"
-            class="w-20 h-28 object-cover rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300" />
-        <img src="https://cdn.myanimelist.net/images/anime/11/39717.jpg" alt="Code Geass"
-            class="w-20 h-28 object-cover rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300" />
-        <img src="https://cdn.myanimelist.net/r/140x220/images/anime/1286/99889.jpg?s=5cfd77fe9bee5552655c9488087b906f"
-            alt="Demon Slayer"
-            class="w-20 h-28 object-cover rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300" />
-        <img src="https://cdn.myanimelist.net/r/140x220/images/anime/1347/117616.jpg?s=259b883350273ab09390ab6ea1d54f09"
-            alt="Your King"
-            class="w-20 h-28 object-cover rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300" />
-        <img src="https://cdn.myanimelist.net/images/anime/1160/122627.jpg" alt="Chainsaw Man"
-            class="w-20 h-28 object-cover rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300" />
-        <img src="https://cdn.myanimelist.net/images/anime/7/76014.jpg" alt="One Punch Man"
-            class="w-20 h-28 object-cover rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300" />
-        <img src="https://cdn.myanimelist.net/r/140x220/images/anime/1974/98158.jpg?s=4b69cc55e00ac874b7d565657f4e686e"
-            alt="Fate/Stay night"
-            class="w-20 h-28 object-cover rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300" />
+    <div class="flex gap-5 justify-center items-center">
+        @foreach ($topRatedSeries as $series)
+            <div class="flex flex-col gap-2 transition-transform duration-200 hover:scale-105">
+                <img src="{{ asset($series->cover_image) }}"
+                    alt="{{ $series->title }}"class="h-30 w-auto mx-auto rounded-md shadow-sm" />
+                <div class="flex gap-2 justify-center items-center">
+                    <div class="font-bold">{{ $series->pivot->score }}</div>
+                    <img src="{{ asset('storage/images/star.svg') }}" class="w-7 h-7" alt="Star Icon">
+                </div>
+            </div>
+        @endforeach
     </div>
 </div>
