043820587b3bc4fd229626a428575398279fd980
Angelo van Osch <Angelo.van.osch@hotmail.com>
2025-08-02 03:05:46 +0200

    routing

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SeriesController extends Controller
+{
+    public function index() {}
+
+    public function show() {}
+
+    public function update() {}
+
+    public function destroy() {}
+}
